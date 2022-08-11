fc518af98f420e3e280bcf528b2ec171410a4d52
Tim Ludwig <timcat.tl@gmail.com>
2022-08-11 16:12:12 +0200

    setup from fen parsing

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
 use crate::piece::Piece;
+use std::str::FromStr;
 
 pub struct Board {
     squares: [Piece; 64]
@@ -7,5 +8,13 @@ pub struct Board {
 impl Board {
     pub fn new() -> Board {
         Board{ squares: [Piece::None; 64] }
+    }
+}
+
+impl FromStr for Board {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Board, Self::Err> {
+        todo!()
     }
 }
