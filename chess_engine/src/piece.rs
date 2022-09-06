2251760b4003a7254698898a1d414b15e2d59c9e
Tim Ludwig <tim.ludwig@smail.inf.h-brs.de>
2022-09-06 10:00:49 +0200

    Tests for Piece, PieceType, Color

--- chess_engine/src/piece.rs
+++ chess_engine/src/piece.rs
@@ -125,3 +125,58 @@ impl Display for Piece {
         write!(f, "{}", self.figurine())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::piece::{Color, Piece, PieceType};
+
+    #[test]
+    fn color_from_fen() {
+        assert_eq!(Color::White, Color::from_fen_char(&'R'));
+        assert_eq!(Color::White, Color::from_fen_char(&'K'));
+        assert_eq!(Color::White, Color::from_fen_char(&'P'));
+        assert_eq!(Color::Black, Color::from_fen_char(&'r'));
+        assert_eq!(Color::Black, Color::from_fen_char(&'k'));
+        assert_eq!(Color::Black, Color::from_fen_char(&'p'));
+    }
+
+    #[test]
+    fn type_from_fen() {
+        assert_eq!(PieceType::King, PieceType::from_fen_char(&'K').unwrap());
+        assert_eq!(PieceType::King, PieceType::from_fen_char(&'k').unwrap());
+        assert_eq!(PieceType::Rook, PieceType::from_fen_char(&'R').unwrap());
+        assert_eq!(PieceType::Rook, PieceType::from_fen_char(&'r').unwrap());
+        assert_eq!(PieceType::Queen, PieceType::from_fen_char(&'Q').unwrap());
+        assert_eq!(PieceType::Queen, PieceType::from_fen_char(&'q').unwrap());
+    }
+
+    #[test]
+    fn piece_from_fen() {
+        assert_eq!(Piece { color: Color::White, piece_type: PieceType::King }, Piece::from_fen_char(&'K').unwrap());
+        assert_eq!(Piece { color: Color::Black, piece_type: PieceType::Rook }, Piece::from_fen_char(&'r').unwrap());
+        assert_eq!(Piece { color: Color::White, piece_type: PieceType::Queen }, Piece::from_fen_char(&'Q').unwrap());
+        assert_eq!(Piece { color: Color::Black, piece_type: PieceType::King }, Piece::from_fen_char(&'k').unwrap());
+        assert_eq!(Piece { color: Color::White, piece_type: PieceType::Pawn }, Piece::from_fen_char(&'P').unwrap());
+        assert_eq!(Piece { color: Color::Black, piece_type: PieceType::Bishop }, Piece::from_fen_char(&'b').unwrap());
+    }
+
+    #[test]
+    fn fen_from_piece() {
+        assert_eq!('K', Piece { color: Color::White, piece_type: PieceType::King }.fen_char());
+        assert_eq!('r', Piece { color: Color::Black, piece_type: PieceType::Rook }.fen_char());
+        assert_eq!('Q', Piece { color: Color::White, piece_type: PieceType::Queen }.fen_char());
+        assert_eq!('k', Piece { color: Color::Black, piece_type: PieceType::King }.fen_char());
+        assert_eq!('P', Piece { color: Color::White, piece_type: PieceType::Pawn }.fen_char());
+        assert_eq!('b', Piece { color: Color::Black, piece_type: PieceType::Bishop }.fen_char());
+    }
+
+    #[test]
+    fn fig_from_piece() {
+        assert_eq!('♚', Piece { color: Color::White, piece_type: PieceType::King }.figurine());
+        assert_eq!('♖', Piece { color: Color::Black, piece_type: PieceType::Rook }.figurine());
+        assert_eq!('♛', Piece { color: Color::White, piece_type: PieceType::Queen }.figurine());
+        assert_eq!('♔', Piece { color: Color::Black, piece_type: PieceType::King }.figurine());
+        assert_eq!('♟', Piece { color: Color::White, piece_type: PieceType::Pawn }.figurine());
+        assert_eq!('♗', Piece { color: Color::Black, piece_type: PieceType::Bishop }.figurine());
+    }
+}
